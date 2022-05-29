888d9f9e14d910b42bb08a0f9d3d801316fac541
zahraghasemip <zahraghasemi2487@gmail.com>
2022-05-29 13:47:48 +0430

    fixed a bug & clean const require(util/types) in webserver.js

--- webserver.js
+++ webserver.js
@@ -3,7 +3,6 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const path = require("path");
-const { isStringObject } = require("util/types");
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 const utils = require("./utils/utils");
